8ffd35949c5b39d8fd24cc3ea7a267edaf602307
Eric Youngberg <eric@lmtlss.net>
2019-12-31 10:18:30 -0500

    Fix improper offset magnitudes for scrubber viewport

--- src/components/Scrubber/index.jsx
+++ src/components/Scrubber/index.jsx
@@ -16,12 +16,12 @@ class Scrubber extends React.Component {
     render() {
         const { blocks, viewport } = this.props.store;
 
-        const offset = viewport.left <= blocks.extent.left ? 'left' : 'right';
+        const offset = viewport.right >= blocks.extent.right ? 'right' : 'left';
 
         const scrubber = {
-            width: `${100 * viewport.width / Math.round(blocks.extent.right - blocks.extent.left)}%`,
+            width: `${100 * viewport.width / Math.abs(blocks.extent.right - blocks.extent.left)}%`,
             height: `${100}%`,
-            [offset]: `${100 * (viewport[offset] - blocks.extent[offset]) / viewport.width}%`,
+            [offset]: `${100 * (viewport[offset] - blocks.extent[offset]) / Math.abs(blocks.extent.right - blocks.extent.left)}%`,
         };
 
         return (
