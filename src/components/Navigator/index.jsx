e6e80746761818f4ccbfdd80894e3c7a8ae292df
Eric Youngberg <eyoungberg@sasaki.com>
2020-07-13 14:06:41 -0400

    Enable click events on navigator

--- src/components/Navigator/index.jsx
+++ src/components/Navigator/index.jsx
@@ -15,6 +15,27 @@ import BlockVisualizer from './BlockVisualizer';
 @observer
 class Navigator extends React.Component {
 
+    onClick = ({ target, clientX, clientY }) => {
+        if (this.ref) {
+            const { blocks, viewport } = this.props.store;
+            const { left, top, width, height } = this.ref.getBoundingClientRect();
+
+            const xPos = clientX - left;
+            const yPos = clientY - top;
+
+            const xValue = (xPos / width) * blocks.extent.width;
+            const yValue = (yPos / height) * blocks.extent.height;
+            const halfwidth = viewport.width / 2;
+
+            console.log(xValue, halfwidth);
+
+            viewport.setLeft(xValue - halfwidth);
+            viewport.setRight(xValue + halfwidth);
+
+            viewport.setTop(yValue - viewport.height / 2);
+        }
+    }
+
     render() {
         const { blocks, viewport } = this.props.store;
         const { extent } = blocks;
@@ -28,7 +49,11 @@ class Navigator extends React.Component {
         };
 
         return (
-            <div className="react-timeline__navigator">
+            <div
+                ref={el => this.ref = el}
+                className="react-timeline__navigator"
+                onClick={e => this.onClick(e)}
+            >
                 <div
                     className="react-timeline__navigator-viewport"
                     style={navigator}
