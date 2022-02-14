065b7669931908648f1079b5c70165c0d1f6f687
arminakvn <armin.akhavan@gmail.com>
2022-02-14 08:31:30 -0500

    IMP-127 Implement updated functionality for TimelinePreview

--- src/types/block.js
+++ src/types/block.js
@@ -4,7 +4,7 @@
  * Stores information regarding time blocks.
  */
 
-const { v4: uuidv4 } = require('uuid');
+import { v4 as uuidv4 } from 'uuid';
 import { action, computed, observable } from 'mobx';
 
 
