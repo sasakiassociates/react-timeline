1063324215aa853e1601d36ba5b88e1734f9f753
Eric Youngberg <eyoungberg@sasaki.com>
2022-05-17 09:06:02 -0400

    Upgrade build. Move to typescript

--- rollup.config.js
+++ rollup.config.js
@@ -1,38 +1,39 @@
-import babel from 'rollup-plugin-babel'
-import commonjs from 'rollup-plugin-commonjs'
-import postcss from 'rollup-plugin-postcss'
-import resolve from 'rollup-plugin-node-resolve'
-import replace from 'rollup-plugin-replace'
+import peerDepsExternal from 'rollup-plugin-peer-deps-external';
+import commonjs from '@rollup/plugin-commonjs';
+import { nodeResolve } from '@rollup/plugin-node-resolve';
+import postcss from 'rollup-plugin-postcss';
+import typescript from '@rollup/plugin-typescript';
 
 import pkg from './package.json'
 
 export default {
-  input: 'src/index.jsx',
-  output: [
-    {
-      file: pkg.main,
-      format: 'cjs',
-      sourcemap: true
+
+    input: 'src/index.ts',
+
+    output: [
+        {
+            file: pkg.main,
+            format: 'cjs',
+            sourcemap: true
+        },
+        {
+            file: pkg.module,
+            format: 'es',
+            sourcemap: true
+        }
+    ],
+
+    plugins: [
+        peerDepsExternal(),
+        nodeResolve({ preferBuiltins: true }),
+        commonjs(),
+        postcss({ plugins: [] }),
+        typescript(),
+    ],
+
+    onwarn(warning) {
+        if (warning.code !== 'THIS_IS_UNDEFINED') {
+            console.warn(warning.message);
+        }
     },
-    {
-      file: pkg.module,
-      format: 'es',
-      sourcemap: true
-    }
-  ],
-  external: id => /^react/.test(id),
-  plugins: [
-    replace({
-        'process.env.NODE_ENV': JSON.stringify(process.env.NODE_ENV),
-    }),
-    postcss(),
-    babel({
-      exclude: 'node_modules/**',
-    }),
-    resolve({
-        preferBuiltins: true,
-        extensions: ['.js', '.jsx'],
-    }),
-    commonjs(),
-  ]
 }
