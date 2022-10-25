2a39c941980ee69805a43fa02c1d2db18fc0a0ab
Eric Youngberg <eyoungberg@sasaki.com>
2022-10-25 13:19:56 -0400

    Default BlockState context

--- src/context.ts
+++ src/context.ts
@@ -6,5 +6,5 @@ export const TimelineContext = createContext<TimelineStore>(new TimelineStore())
 export const useTimeline = () => useContext<TimelineStore>(TimelineContext);
 
 import BlockState from './models/BlockState';
-export const BlockContext = createContext<BlockState>(new BlockState());
+export const BlockContext = createContext<BlockState>(new BlockState(new TimelineStore()));
 export const useBlock = () => useContext<BlockState>(BlockContext);
